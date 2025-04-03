4ccabdb0b0dacc398c46644d429f65a846aadc6e
rizkywahyudi <rizkywahyudi302008@gmail.com>
2025-04-03 20:12:15 +0700

    update and delete item

---
+++ js/lirik.js
@@ -0,0 +1,34 @@
+// Contoh objek dengan lirik lagu
+const lyricsData = [
+  {
+    src: "beliCiki-Koiiri", // pastikan cocok dengan src di music-list.js
+    lyrics: `
+    Beli Ciki
+    Beli koyo
+    
+    Sukiyo
+    Ima anata ni omoi nosete hora
+    Sunao ni naru no watashi
+    Kono saki motto
+    Sobani ite moikana
+    Koi to koi ga kasanate
+    
+    Sukiyo..`,
+  },
+  {
+    src: "reff-aduRayu",
+    lyrics: `
+    Maukah lagi kau mengulang ragu
+    Dan sendu yang lama
+    Dia yang dulu pernah bersamamu
+    Memahat kecewa
+    Atau kau inginkan yang baru
+    Sungguh menyayangimu
+    
+    Aku ingin dirimu
+    Yang menjadi milikku
+    Bersamaku mulai hari ini
+    Hilang ruang untuk cinta yang lain`,
+  },
+  // Tambahkan lirik untuk lagu lainnya
+];
